9c9231cff571d47c472fc0819ed7322ea4244204
rebeccavanni <ecn2wh@virginia.edu>
2024-10-22 00:56:44 +0000

    step 1 of lab six, adding changes to app

--- app/main.py
+++ app/main.py
@@ -10,8 +10,12 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Bye": "Bye API"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/square/{a}")
+def square(a: int):
+    return {"sum": a * a}

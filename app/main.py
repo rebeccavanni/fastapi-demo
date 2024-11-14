4bea58dcee03c32e343c27c586290c64eaccf43a
rebeccavanni <ecn2wh@virginia.edu>
2024-11-14 18:21:24 +0000

    porject work

--- app/main.py
+++ app/main.py
@@ -5,22 +5,68 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "ecn2wh"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Bye": "Bye API"}
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
-@app.get("/square/{a}")
-def square(a: int):
-    return {"sum": a * a}
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title AS title,
+        songs.album AS album,
+        songs.artist AS artist,
+        songs.year AS year,
+        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.mp3_file) AS file,
+        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.image_file) AS image,
+        genres.genre AS genre
+    FROM 
+        songs
+    JOIN 
+        genres ON songs.genre = genres.genreid
+    ORDER BY 
+        songs.songid;
+    """
+    try:    
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
-@app.get("/subtract/{a}/{b}")
-def subtract(a: int, b: int):
-    return {"minus": a - b}
 

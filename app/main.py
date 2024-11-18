e990821eebc00434a82521f00891c4ee58bc3614
rebeccavanni <ecn2wh@virginia.edu>
2024-11-18 19:04:24 +0000

    adding changes

--- app/main.py
+++ app/main.py
@@ -20,8 +20,65 @@ db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database
 cur=db.cursor()
 
 app = FastAPI()
-
+app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+@app.get('/songs')
+def get_songs():
+    query = "SELECT * FROM songs ORDER BY songid;"
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": f"MySQL Error: {str(e)}"}
+
+@app.get('/songs/details')
+def get_song_details():
+    query = """
+        SELECT 
+            s.title AS title, 
+            s.album AS album, 
+            s.artist AS artist, 
+            s.year AS year, 
+            CONCAT('https://your-s3-bucket-url/', s.file) AS mp3_url, 
+            CONCAT('https://your-s3-bucket-url/', s.image) AS image_url, 
+            g.name AS genre
+        FROM songs s
+        JOIN genres g ON s.genre = g.genreid
+        ORDER BY s.songid;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": f"MySQL Error: {str(e)}"}
+
 
 
 

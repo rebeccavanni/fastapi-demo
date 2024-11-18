079d892aefef0ac376085715c6a976cd958dc802
rebeccavanni <ecn2wh@virginia.edu>
2024-11-18 18:26:49 +0000

    mid project commit

--- app/main.py
+++ app/main.py
@@ -3,14 +3,17 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+
 import json
 import os
 import mysql.connector
 from mysql.connector import Error
 
-DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
-DBUSER = "admin"
-DBPASS = os.getenv('DBPASS')
+from dotenv import load_dotenv
+load_dotenv()
+DBHOST = os.getenv("Database_Host")
+DBUSER = os.getenv("Database_User")
+DBPASS = os.getenv("Database_Pass")
 DB = "ecn2wh"
 
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
@@ -19,54 +22,6 @@ cur=db.cursor()
 app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-@app.get('/genres')
-def get_genres():
-    query = "SELECT * FROM genres ORDER BY genreid;"
-    try:    
-        cur.execute(query)
-        headers=[x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data=[]
-        for result in results:
-            json_data.append(dict(zip(headers,result)))
-        return(json_data)
-    except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
-
-@app.get('/songs')
-def get_songs():
-    query = """
-    SELECT 
-        songs.title AS title,
-        songs.album AS album,
-        songs.artist AS artist,
-        songs.year AS year,
-        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.mp3_file) AS file,
-        CONCAT('https://s3.amazonaws.com/your-bucket/', songs.image_file) AS image,
-        genres.genre AS genre
-    FROM 
-        songs
-    JOIN 
-        genres ON songs.genre = genres.genreid
-    ORDER BY 
-        songs.songid;
-    """
-    try:    
-        cur.execute(query)
-        headers = [x[0] for x in cur.description]
-        results = cur.fetchall()
-        json_data = []
-        for result in results:
-            json_data.append(dict(zip(headers, result)))
-        return json_data
-    except Error as e:
-        return {"Error": "MySQL Error: " + str(e)}
 
 
